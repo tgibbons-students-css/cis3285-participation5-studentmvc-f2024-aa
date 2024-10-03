e8a6cf81702b9ea1d512ea681541b9cd843b1ae7
allielopez03 <alopez2@css.edu>
2024-10-03 11:47:17 -0500

    Got to part 4

---
+++ CIS3285_Unit4_StudentMVC_2024/Models/IStudentInterface.cs
@@ -0,0 +1,24 @@
+﻿namespace CIS3285_Unit4_StudentMVC_2024.Models
+{
+    public interface IStudentInterface
+    {
+        int Credits { get; set; }
+        int Id { get; set; }
+        string Name { get; set; }
+
+        public IStudentInterface (int id, string name, int credits)
+        {
+            Id = id;
+            Name = name;
+            Credits = credits;
+        }
+
+
+
+
+
+
+
+
+    }
+}
